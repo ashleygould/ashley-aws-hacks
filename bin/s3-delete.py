9d4a6a64c07554b4062db96851bd8a6c2db6ddb3
Ashley Gould <agould@ucop.edu>
2017-12-13 13:41:45 -0800

    add s3-delete.py

---
+++ bin/s3-delete.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""Delete all objects in a versioned bucket and delete the bucket"""
+import sys
+import boto3
+
+if len(sys.argv) < 1:
+    print('provide a bucket name')
+bucket_name = sys.argv[1]
+print('deleting bucket {}'.format(bucket_name))
+s3 = boto3.resource('s3')
+bucket = s3.Bucket(bucket_name)
+bucket.object_versions.delete()
+bucket.delete()
