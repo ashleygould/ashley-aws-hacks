05cab3228bfc44933f142b5fa730199f06405bc8
Ashley Gould <agould@ucop.edu>
2019-04-26 11:55:15 -0700

    add --private flag to r53-record

--- bin/r53-record.py
+++ bin/r53-record.py
@@ -4,7 +4,7 @@ query record sets in a hosted zone by name or type
 
 Usage:
     r53-record (-hv)
-    r53-record [-t TYPE] [-n NAME] [-r REGION] DOMAINNAME
+    r53-record [-p] [-t TYPE] [-n NAME] [-r REGION] DOMAINNAME
 
 
 
@@ -14,6 +14,7 @@ Options:
     -t, --type TYPE      Type of DNS record.
     -n, --name NAME      Name of DNS record.
     -r, --region REGION  AWS region.
+    -p, --private        Hosted zone is private
     DOMAINNAME           The DNS domain name of the hosted zone to query.
 
 """
@@ -23,15 +24,44 @@ import json
 import yaml
 import boto3
 from docopt import docopt
-from sceptremods.util.acm import get_hosted_zone_id
+
 
 __version__ = '0.0.0'
 DEFAULT_REGION = 'us-west-2'
 TYPES = ['SOA', 'A', 'TXT', 'NS', 'CNAME', 'MX', 'NAPTR', 'PTR', 'SRV', 'SPF', 'AAAA', 'CAA']
 
+
+def get_hosted_zone_id(domain_name, private_zone=False, region=DEFAULT_REGION):
+    """
+    Return the hosted zoned Id corresponding to 'domain_name'.
+    """
+    route53_client = boto3.client('route53', region_name=region)
+    response = route53_client.list_hosted_zones()
+    hosted_zones = response["HostedZones"]
+    while response["IsTruncated"]:
+        response = route53_client.list_hosted_zones(
+            Marker=reponse["NextMarker"]
+        )
+        hosted_zones += response["HostedZones"]
+    if not domain_name.endswith("."):
+        domain_name += "."
+    hosted_zone_ids = [
+        zone['Id'] for zone in hosted_zones if
+        zone['Name'] == domain_name and
+        zone['Config']['PrivateZone'] is private_zone
+    ]
+    if len(hosted_zone_ids) > 1:
+        raise RuntimeError(
+            "Found multiple matching hosted zones: {}".format(cert_list)
+        )
+    if len(hosted_zone_ids) < 1:
+        return None
+    return hosted_zone_ids[0].split("/")[2]
+
+
 def main():
     args = docopt(__doc__, version=__version__)
-    #print(args)
+    print(args)
 
     if not args['--region']:
         region = DEFAULT_REGION
@@ -44,7 +74,7 @@ def main():
         )
 
     client = boto3.client('route53', region_name=region)
-    hosted_zone_id = get_hosted_zone_id(args['DOMAINNAME'], region)
+    hosted_zone_id = get_hosted_zone_id(args['DOMAINNAME'], args['--private'], region)
     #print(hosted_zone_id)
 
     response = client.list_resource_record_sets(HostedZoneId=hosted_zone_id)
