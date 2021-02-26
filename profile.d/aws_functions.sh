750be1e2bcc6416d82de4a480007d289831adaf8
Ashley Gould <agould@ucop.edu>
2021-02-26 10:57:19 -0800

    new func: ssm-param-put-overwrite

--- profile.d/aws_functions.sh
+++ profile.d/aws_functions.sh
@@ -489,8 +489,14 @@ ssm-param-get-verbose() {
     aws ssm get-parameter --name $1
 }
 
+# for values which are URLs run `aws configure set cli_follow_urlparam false`
+# to prevent aws-cli attepting resolve the value from the internet
 ssm-param-put() {
     aws ssm put-parameter --name $1 --value $2 --type String
+}
+
+ssm-param-put-overwrite() {
+    aws ssm put-parameter --name $1 --value $2 --type String --overwrite
 }
 
 ssm-param-delete() {
